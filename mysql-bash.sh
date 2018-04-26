702f0c1eb872931e63e0ab5d5b31ea4bdf1475ce
Kate.An <uv_vu@live.cn>
2018-04-26 14:46:15 +0800

    add mysql bash

--- mysql-bash.sh
+++ mysql-bash.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 # mysql commti
 #mysql path
-$host = ""
-$user = ""
-$password = ""
-mysql -h $host -u $user -p$password
+host="amazom.cxq7foc1svrl.ap-northeast-1.rds.amazonaws.com"
+user="amaroot"
+password="amazon.18"
+mysql -h $host -u $user -p$password <<EOF
+show databases;
+use mysql;
+show tables;
+select * from user
+EOF
+exit;
