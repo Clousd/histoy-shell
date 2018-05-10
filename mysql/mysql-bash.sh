757f1975c085cd128e2efe1230884bbe3736c094
Kate.An <uv_vu@live.cn>
2018-05-10 16:26:40 +0800

    123

--- mysql/mysql-bash.sh
+++ mysql/mysql-bash.sh
@@ -3,11 +3,23 @@
 #mysql path
 host="amazom.cxq7foc1svrl.ap-northeast-1.rds.amazonaws.com"
 user="amaroot"
-password=""
+password="amazon.18"
 mysql -h $host -u $user -p$password <<EOF
+select "\n\n\n";
 show databases;
+select "\n\n\n";
 use mysql;
+
+
+
+
+
+
+
 show tables;
+
+
+
 select * from user
 EOF
 exit;
