2a5358294631e1b7b68dd151f9dde467859d1644
Kate.An <36059376+Clousd@users.noreply.github.com>
2018-05-10 10:34:48 +0800

    Update install_rpm.sh

--- mysql/install_rpm.sh
+++ mysql/install_rpm.sh
@@ -5,10 +5,10 @@ yum install  https://repo.mysql.com//mysql80-community-release-el7-1.noarch.rpm 
 yum update -y
 yum-config-manager --disable mysql80-community
 yum-config-manager --enable mysql57-community
-echo 1.mysql55-community/x86_64(@)
-echo 2.mysql56-community/x86_64(#)
-echo 3.mysql57-community/x86_64(&)
-echo 3.mysql58-community/x86_64(&)
+echo "1.mysql55-community/x86_64(@)"
+echo "2.mysql56-community/x86_64(#)"
+echo "3.mysql57-community/x86_64(&)"
+echo "3.mysql58-community/x86_64(&)"
 echo Please choose:
 read -n 1 M
 #下面一行是换行
