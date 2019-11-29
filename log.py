1611060f2e97feedafdba78012bb0207792cb232
kate.an <kate.an@xdge-auto.com>
2019-11-29 14:07:30 +0800

    123

---
+++ log.py
@@ -0,0 +1,22 @@
+#!/bin/bash
+#tail  -n 999900 /usr/local/apache2/logs/access_log |grep 28/Jui
+#113.143.180.248 - - image.zhaoits.com [09/Sep/2019:15:05:22 +0800] "GET /upload/AppIcon/System/1517534895-5a73beaff0430.png HTTP/1.1" "-"  "Mozilla/5.0 (iPhone; CPU iPhone OS 12_4_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148" 200 4602
+#from pandas  import DataFrame  as dd
+#file = open(u'access_log')
+#line = file.readline()
+#dds = dd(['ip','url'],[line.split()[0],line.split()[2]])
+#print(dds)
+#for t in tt:
+ #   print (t)
+#print(tt)
+#while 1:
+#    iffin=1
+#    lines = file.readlines(1000000)
+#    if  iffin==1:
+#       if findvchar(lines)!='':
+#           print   ()
+#    if not lines:
+ #     break
+ #   
+ #     if u'Nov/2019' in lines:
+ #       iffin==2
